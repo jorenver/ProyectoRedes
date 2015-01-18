2a772e5eb1d9d68e06e261f2a487835daaacec2a
rodrigo <rodfcast@espol.edu.ec>
2015-01-18 15:50:11 -0500

    Se muestran direccions MAC e Ip de los host origen y destino del mensaje ARP

--- captura.c
+++ captura.c
@@ -8,6 +8,7 @@
 
 #include <netinet/if_ether.h>
 #include <netinet/ether.h>
+
 #include <net/ethernet.h>
 #include <net/if_arp.h>
 
@@ -26,16 +27,40 @@ void my_callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char* 
 	struct ether_arp *h_arp;
 	int size_ethernet=sizeof(struct ether_header);//tamaño de la cabezera ethernet
 	h_ethernet = (struct ether_header *) packet;//Apuntamos a la cabezera Ethernet que esta al comienzo de packet
-	printf("Paquete numero: %d\n",count);
-	printf("MAC origen: %s\n", ether_ntoa((struct ether_addr *)h_ethernet->ether_shost));
-	printf("MAC destino: %s\n", ether_ntoa((struct ether_addr *)h_ethernet->ether_dhost) );
+	printf("\nPacket Number: %d\n",count);
+	printf("MAC source: %s\n", ether_ntoa((struct ether_addr *)h_ethernet->ether_shost));
+	printf("MAC destination: %s\n", ether_ntoa((struct ether_addr *)h_ethernet->ether_dhost) );
 
-	h_arp=(struct ether_arp*)(packet+size_ethernet);//se apunta al paquete arp
+	h_arp=(struct ether_arp*)(packet+size_ethernet);
+	/*
+	(packet+size_ethernet) da la direccion base dentro del paquete en donde empieza el mensaje arp
+	ether_arp tiene un campo de tipo arphd llamado ea_hdr: este campo contiene la informacion de tamaño fija dentro del mensaje
+	como: Tipo y longitud de hardware, tipo y longitud de protocolo y op_code: operacion(ar_op) que pueden ser REQUEST o REPLY
+	*/
 	if(ntohs(h_arp->ea_hdr.ar_op)==ARP_REQUEST){
 		printf("ARP Request\n");
 	}else{
 		printf("ARP REPLY\n");
 	}
+	/*
+	char *ether_ntoa (const struct ether_addr *argumento);esta funcion convierte la direccion MAC contenida en "argumento" en un string leible
+	char *inet_ntoa (struct in_addr argumento); esta funcion convierte la direccion IP contenida en "argumento" en un string leible 
+	in_addr es un entero de 32 bits sin signo (uint32_t) 
+	en ether_arp las direcciones IP estan definidas como un arreglo de 4 elementos, cada uno de tipo u_int8_t 
+	Hay que enviarle el primer elemento de ese arreglo a la funcion inet_ntoa para que se realize la conversion correctamentes
+	*/
+
+	printf("Sender MAC addres %s\n",ether_ntoa((struct ether_addr*)h_arp->arp_sha));
+	/* la instruccion : *( (struct in_addr*) h_arp->arp_spa) es lo mismo que
+	struct in_addr *direccion_ip=(struct in_addr*)h_arp->arp_spa
+	//struct in_addr contenido_dir=*direccion_ip;
+	//inet_ntoa(contenido_dir);
+	*/
+	printf("Sender IP addres %s\n", inet_ntoa(*( (struct in_addr*) h_arp->arp_spa) ));
+	
+	printf("Target MAC addres %s\n",ether_ntoa((struct ether_addr*)h_arp->arp_tha));
+
+	printf("Target IP addres %s\n", inet_ntoa(*( (struct in_addr*) h_arp->arp_tpa) ));
 
 }
 
