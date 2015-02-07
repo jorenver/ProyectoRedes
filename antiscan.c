154ad9d58e473e7f1ca7fcecb80f58c18f77dfc4
obayona <obayona@espol.edu.ec>
2015-02-06 19:20:30 -0500

    Enviar Reply falla

--- antiscan.c
+++ antiscan.c
@@ -10,6 +10,7 @@
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <net/ethernet.h>
+#include <netinet/ether.h>
 #include <net/if_arp.h>
 
 #include <string.h>
@@ -22,11 +23,11 @@ typedef enum{ REQUEST=1, REPLY} ARPtype;
 
 
 void listarInterfaces();
-int enviarReply(pcap_t * pcap, u_int8_t senderMac,u_int8_t senderIP,u_int8_t miMac,u_int8_t destinationIP);
+int enviarReply(pcap_t * pcap, u_int8_t *miMac,u_int8_t *senderIP, u_int8_t *destinationMac,u_int8_t *destinationIP);
 
 //variable global
-char* targetIP = "127.0.0.1";   //IP del dispositivo target, localhost por defecto
-
+char* targetIP = "10.0.2.6";   //IP del dispositivo target, localhost por defecto
+char * mimac = "08:00:27:ca:eb:7e"; //Mac del target
 
 void callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char* packet){
   static int count = 1;
@@ -48,12 +49,18 @@ void callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char* pac
     //strcpy(senderIp,inet_ntoa(*((struct in_addr*)h_arp->arp_spa) ));//produce error???
     type=ntohs(h_arp->ea_hdr.ar_op);
 
-    printf("%s\n",destinationIP);
+    printf("destination: %s, targetIP %s\n",destinationIP, targetIP);
     printf("%d\n\n", type);
 
     //si el arp va dirigido a target y es request envia el arp reply
     if (strcmp(targetIP,destinationIP)==0 && type == REQUEST){
-         //enviarReply(senderMac, senderIP,miMac, destinationIP);
+       printf("*******ARP******");
+       pcap_t* descr;       
+       u_int8_t *miMac;
+       miMac=(u_int8_t*)malloc(sizeof(u_int8_t)*ETH_ALEN);
+       struct ether_addr* Mac = (struct ether_addr*)ether_aton("08:00:27:ca:eb:7e");
+       miMac = Mac->ether_addr_octet; 
+       enviarReply(descr,miMac, h_arp->arp_tpa, h_arp->arp_sha, h_arp->arp_spa);
     }
 
   }
@@ -170,35 +177,50 @@ void listarInterfaces(){
 
 
 
-int enviarReply(pcap_t * pcap, u_int8_t senderMac,u_int8_t senderIP,u_int8_t miMac,u_int8_t destinationIP){
-     //construccion de la cabezera ethernet
-     struct ether_header ether_h;
-     ether_h.ether_type = htons(ETHERTYPE_ARP);
-     memset(ether_h.ether_dhost,senderMac,sizeof(ether_h.ether_dhost));
-     memset(ether_h.ether_shost,miMac, sizeof(ether_h.ether_shost));
-     //construccion del arp reply
-     struct ether_arp reply;
-     reply.arp_hrd = htons(ARPHRD_ETHER);
-     reply.arp_pro = htons(ETH_P_IP);
-     reply.arp_hln = ETHER_ADDR_LEN;
-     reply.arp_pln = sizeof(in_addr_t);
-     reply.ea_hdr.ar_op = 2;
-     memset(&reply.arp_tha,0,sizeof(reply.arp_tha) );
-     memset(reply.arp_tpa,destinationIP ,sizeof(reply.arp_tpa));
-     memset(reply.arp_spa,senderIP ,sizeof(reply.arp_spa));
-     memset(reply.arp_sha,miMac ,sizeof(reply.arp_sha));
-     memset(reply.arp_tha, senderMac ,sizeof(reply.arp_tha));
-     
-     unsigned char frame[sizeof(struct ether_header) + sizeof(struct ether_arp)];
-     memcpy(frame, &ether_h, sizeof(struct ether_header) );
-     memcpy(frame + sizeof(struct ether_header), &reply,sizeof(struct ether_arp) ); 
-          
-     if(pcap_inject(pcap, frame, sizeof(frame)) == -1){
-        return 0;
 
-     }
+int enviarReply(pcap_t * pcap, u_int8_t *miMac,u_int8_t *senderIP, u_int8_t *destinationMac,u_int8_t *destinationIP){
+  printf("enviarReply\n");
+  //construccion de la cabezera ethernet
+  struct ether_header *ether_h=(struct ether_header*)malloc(sizeof(struct ether_header));
+  ether_h->ether_type = htons(ETHERTYPE_ARP);
+  memcpy(ether_h->ether_shost,miMac,sizeof(ether_h->ether_shost));//source maac
+  memcpy(ether_h->ether_dhost,destinationMac,sizeof(ether_h->ether_dhost));//destination mac
 
-     return 1;
+    //construccion del arp reply
+  struct ether_arp *reply=(struct ether_arp*)malloc(sizeof(struct ether_arp));
+  reply->arp_hrd=htons(ARPHRD_ETHER);
+  reply->arp_pro = htons(ETH_P_IP);
+  reply->arp_hln = ETHER_ADDR_LEN;
+  reply->arp_pln =sizeof(in_addr_t);
+  reply->arp_op =htons(ARPOP_REPLY);
+  memcpy(reply->arp_sha,miMac,sizeof(reply->arp_sha));
+  memcpy(reply->arp_spa,senderIP,sizeof(reply->arp_spa));
+  memcpy(reply->arp_tha,destinationMac,sizeof(reply->arp_tha));
+  memcpy(reply->arp_tpa,destinationIP,sizeof(reply->arp_tpa));
     
+  //construccion del paquete para realizar el ataque     
+  unsigned char frame[sizeof(struct ether_header) + sizeof(struct ether_arp)];
+  memcpy(frame,ether_h, sizeof(struct ether_header) );
+  memcpy(frame + sizeof(struct ether_header),reply,sizeof(struct ether_arp) ); 
+          
+  if(pcap_inject(pcap, frame, sizeof(frame)) == -1){
+    return 0;
+  }
+  return 1;  
+}
+
+
+
+int enviarICMPReply(pcap_t* pcap, u_int8_t* miMac, u_int8_t* senderIP, u_int8_t* destinationMac, u_int8_t* destinationIP  ){
+     
+  printf("enviar ICMP reply\n");
+  //construccion de la cabezera ethernet
+  struct ether_header *ether_h=(struct ether_header*)malloc(sizeof(struct ether_header));
+  ether_h->ether_type = htons(ETHERTYPE_IP);
+  memcpy(ether_h->ether_shost,miMac,sizeof(ether_h->ether_shost));//source maac
+  memcpy(ether_h->ether_dhost,destinationMac,sizeof(ether_h->ether_dhost));//destination mac
+
+  //creacion del paquete IP/ICMP    
+   
 
 }
