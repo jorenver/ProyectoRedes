06464119f2c9145a61001d2ba59fb52cfc877812
rodrigo <rodfcast@espol.edu.ec>
2015-02-10 15:23:12 -0500

    Construccion y envio de icmp echo request y icmp echo reply

--- antiscan.c
+++ antiscan.c
@@ -6,11 +6,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netinet/if_ether.h>
+
 #include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+
 #include <net/ethernet.h>
 #include <netinet/ether.h>
+
 #include <net/if_arp.h>
 
 #include <string.h>
@@ -19,71 +22,82 @@
 //ejecutar ./capturaIcmp icmp 100
 #define LIMITE 100
 
+
 typedef enum{ REQUEST=1, REPLY} ARPtype;
 
 
 void listarInterfaces();
 int enviarReply(pcap_t * pcap, u_int8_t *miMac,u_int8_t *senderIP, u_int8_t *destinationMac,u_int8_t *destinationIP);
-int enviarICMPReply(pcap_t* pcap, u_int8_t* miMac, u_int8_t* senderIP, u_int8_t* destinationMac, u_int8_t* destinationIP);
-unsigned short inChecksum(unsigned short * addr, int len);
+int enviarICMPReply(pcap_t* pcap,u_int8_t* senderMac, u_int32_t senderIp, 
+  u_int8_t* destinationMac, u_int32_t  destinationIp,
+  u_int16_t secuencia,u_int16_t id,u_int16_t check,u_int32_t  gateway,void*payload,int payload_len
+  );
+unsigned short in_cksum(unsigned short *addr, int len);
 
 //variable global
-char* targetIP = "10.0.2.6";   //IP del dispositivo target, localhost por defecto
-char * mimac = "08:00:27:ca:eb:7e"; //Mac del target
+char* targetIP = "192.168.0.103";   //IP del dispositivo target, localhost por defecto
+char * mimac = "e4:d5:3d:11:53:01"; //Mac del target
 pcap_t* descr; 
 
-void callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char* packet){
+void callback(u_char *useless,const struct pcap_pkthdr* header,const u_char* packet){
   static int count = 1;
   int size_eth=sizeof(struct ethhdr); //tamanio de la cabezera
   struct ether_header *h_ethernet;//puntero a la cabezera ethernet
-  struct ether_arp *h_arp;// puntero a la cabezera arp
   h_ethernet = (struct ether_header *) packet;
+  //printf("Paquete %d\n",count);
+  count++;
+
+  u_int8_t *miMac;
+  miMac=(u_int8_t*)malloc(sizeof(u_int8_t)*ETH_ALEN);
+  struct ether_addr* Mac = (struct ether_addr*)ether_aton(mimac);
+  miMac = Mac->ether_addr_octet; 
+
 
   if (ntohs(h_ethernet->ether_type)== ETHERTYPE_ARP){
      //analisis arp
+    struct ether_arp *h_arp;
     h_arp=(struct ether_arp*)(packet+size_eth);
-    char*senderMac=(char*)malloc(sizeof(char)*18);
-    char*senderIp=(char*)malloc(sizeof(char)*18);
-    char* destinationIP=(char*)malloc(sizeof(char)*18);
+    char* destinationIp=(char*)malloc(sizeof(char)*18);
     ARPtype type;
 
-    strcpy(destinationIP,inet_ntoa(*( (struct in_addr*) h_arp->arp_tpa) ));
+    strcpy(destinationIp,inet_ntoa(*( (struct in_addr*) h_arp->arp_tpa) ));//la ip del target
     //strcpy(senderMac,(ether_ntoa((struct ether_addr*)h_arp->arp_sha))); //produce error???
     //strcpy(senderIp,inet_ntoa(*((struct in_addr*)h_arp->arp_spa) ));//produce error???
     type=ntohs(h_arp->ea_hdr.ar_op);
 
-    //printf("destination: %s, targetIP %s\n",destinationIP, targetIP);
-    //printf("%d\n\n", type);
+    if (type == REQUEST && strcmp(targetIP,destinationIp)==0){    
+      printf("\nDEBUG: ARP REQUEST al Target\n");
+      enviarReply(descr,miMac, h_arp->arp_tpa, h_arp->arp_sha, h_arp->arp_spa);
 
-    //si el arp va dirigido a target y es request envia el arp reply
-    if (type == REQUEST && strcmp(destinationIP,targetIP)==0){
-       printf("*******ARP******");    
-       u_int8_t *miMac;
-       miMac=(u_int8_t*)malloc(sizeof(u_int8_t)*ETH_ALEN);
-       struct ether_addr* Mac = (struct ether_addr*)ether_aton("08:00:27:ca:eb:7e");
-       miMac = Mac->ether_addr_octet; 
-       enviarReply(descr,miMac, h_arp->arp_tpa, h_arp->arp_sha, h_arp->arp_spa);;
+    }
+  }else{ //si no se arp es un paquete IP
+    struct iphdr *iph;//puntero a la cabezera ip
+    char *destinationIp;
+    iph=(struct iphdr*)(packet+size_eth);   
+    
+    int size_ip=(iph->ihl)*4;//tamaño de cabezera ip  
+    struct in_addr *dir=malloc(sizeof(struct in_addr));
+    dir->s_addr=iph->daddr;
+    destinationIp=inet_ntoa(*dir);//destino del icmp packet
+
+    struct icmphdr *icmph;//puntero a paquete icmp
+    icmph=(struct icmphdr*)(packet+size_eth+size_ip);
+
+    if(icmph->type==ICMP_ECHO&&strcmp(targetIP,destinationIp)==0){
+        printf("DEBUG: ICMP ECHO al Target\n");
+        in_addr_t miIp=inet_addr(targetIP);//mi direccion ip
+        int size=size_eth+size_ip+sizeof(struct icmphdr);
+        int payload_len=header->len - size;
+        void*payload;
+        payload=(void*)(packet+size);
+        int i;
+        enviarICMPReply(descr,miMac,miIp,h_ethernet->ether_shost,iph->saddr,
+                       icmph->un.echo.sequence,icmph->un.echo.id,icmph->checksum,icmph->un.gateway,
+                       payload,payload_len);
+        printf("DEBUG: ICMP ECHO REPLY del Target\n");
     }
 
-  }else{ //si no se arp es un paquete IP
-    
-       struct iphdr *iph=(struct iphdr*)(packet+size_eth);
-       int size_ip=iph->ihl*4;
-       struct icmphdr *icmph=(struct icmphdr*)(packet+size_ip+size_eth);
-
-       if (icmph->type == ICMP_ECHO ){ // si se recibe un ICMP ECHO
-          
-          printf("*********Enviar ICMP*******\n");
-          u_int8_t *miMac;
-          miMac=(u_int8_t*)malloc(sizeof(u_int8_t)*ETH_ALEN);
-          struct ether_addr* Mac = (struct ether_addr*)ether_aton("08:00:27:ca:eb:7e");
-          miMac = Mac->ether_addr_octet; 
-          //enviarICMPReply(descr, miMac,&iph->daddr,&h_ethernet->ether_shost, iph->saddr );  
-          
-       }
-       printf("\nPacket number [%d], length of this packet is: %d\n", count++, pkthdr->len);
   }
-
 }
 
 int main(int argc,char **argv)
@@ -148,14 +162,12 @@ int main(int argc,char **argv)
     // For every packet received, call the callback function
     // For now, maximum limit on number of packets is specified
     // by user.
-    pcap_loop(descr, 100, callback, NULL);
+    pcap_loop(descr,-1, callback, NULL);
 
     printf("\nDone with packet sniffing!\n");
     return 0;
+
 }
-
-
-
 
 
 void listarInterfaces(){
@@ -175,7 +187,7 @@ void listarInterfaces(){
 }
 
 int enviarReply(pcap_t * pcap, u_int8_t *miMac,u_int8_t *senderIP, u_int8_t *destinationMac,u_int8_t *destinationIP){
-  printf("enviarReply\n");
+  printf("DEBUG: ARP REPLY del Target\n");
   //construccion de la cabezera ethernet
   struct ether_header *ether_h=(struct ether_header*)malloc(sizeof(struct ether_header));
   ether_h->ether_type = htons(ETHERTYPE_ARP);
@@ -205,69 +217,102 @@ int enviarReply(pcap_t * pcap, u_int8_t *miMac,u_int8_t *senderIP, u_int8_t *des
   return 1;  
 }
 
-
-
-int enviarICMPReply(pcap_t* pcap, u_int8_t* miMac, u_int8_t* senderIP, u_int8_t* destinationMac, u_int8_t* destinationIP  ){
-     
-  printf("enviar ICMP reply\n");
-  //construccion de la cabezera ethernet
-  struct ether_header *ether_h=(struct ether_header*)malloc(sizeof(struct ether_header));
+int enviarICMPReply(pcap_t* pcap,u_int8_t* senderMac, u_int32_t senderIp, 
+  u_int8_t* destinationMac, u_int32_t  destinationIp,
+  u_int16_t secuencia,u_int16_t id,u_int16_t check,u_int32_t  gateway,void*payload,int payload_len
+  ){
+  //cabezera ethernet
+  int sizeEthernet=sizeof(struct ether_header);
+  struct ether_header *ether_h=(struct ether_header*)malloc(sizeEthernet);
   ether_h->ether_type = htons(ETHERTYPE_IP);
-  memcpy(ether_h->ether_shost,miMac,sizeof(ether_h->ether_shost));//source maac
+  memcpy(ether_h->ether_shost,senderMac,sizeof(ether_h->ether_shost));//source maac
   memcpy(ether_h->ether_dhost,destinationMac,sizeof(ether_h->ether_dhost));//destination mac
-  char * packet;
-  //creacion del paquete IP/ICMP   
-  struct iphdr* ip_header; 
-  struct icmphdr * icmp_header;
-  packet = (char*)malloc(sizeof(struct ether_header) + sizeof(struct iphdr) + 
-                         sizeof(struct icmphdr)); 
 
-  ip_header = (struct iphdr*)packet[sizeof(struct ether_header)];
-  icmp_header = (struct icmphdr*)packet[sizeof(struct ether_header) + sizeof(struct iphdr)];
-  ip_header->ihl = 5;
-  ip_header->version = 4;
-  ip_header->tot_len = sizeof(struct iphdr) + sizeof(struct icmphdr);
-  ip_header->protocol = IPPROTO_ICMP;
-  ip_header->saddr = *senderIP;
-  ip_header->daddr = *destinationIP;
-  ip_header->check = inChecksum((unsigned short*)ip_header, sizeof(struct iphdr));
-  
-  icmp_header->type = ICMP_ECHOREPLY;
-  icmp_header->checksum = inChecksum((unsigned short*)icmp_header,sizeof(struct icmphdr));	
-  
+  //cabezera ip
+  int sizeIp=sizeof(struct iphdr);
+  struct iphdr*ip;
+  ip=(struct iphdr*)malloc(sizeIp);
+  ip->version=4;
+  ip->ihl=(sizeof(struct iphdr))/4;
+  ip->tos=0;
+  ip->tot_len=htons(sizeof(struct iphdr)+sizeof(struct icmphdr)+payload_len*sizeof(u_char));
+  ip->frag_off=0;
+  ip->id=0;
+  ip->saddr=senderIp;
+  ip->daddr=destinationIp;
+  ip->ttl=64;
+  ip->protocol=IPPROTO_ICMP;
 
-  if(pcap_inject(pcap, packet, sizeof(packet)) == -1){
+  //cabezera icmp
+  int sizeIcmp=sizeof(struct icmphdr);
+  struct icmphdr*icmp;
+  icmp=(struct icmphdr*)malloc(sizeIcmp);
+  icmp->type=htons(ICMP_ECHOREPLY);
+  icmp->code=htons(0);
+  icmp->checksum=0;
+  icmp->un.echo.id=id;
+  icmp->un.echo.sequence=secuencia;
+  icmp->un.gateway=gateway;
+
+  u_char *pdu;
+  pdu=(u_char*)malloc(payload_len*sizeof(u_char));
+  int i;
+  for(i=0;i<payload_len;i++){
+    *(pdu+i)=*((u_char*)payload+i);
+  }
+
+  //calculo de checksum para icmp
+  u_char*tmp;
+  tmp=(u_char*)malloc(sizeIcmp+payload_len*sizeof(u_char));
+  memcpy(tmp,icmp,sizeIcmp);
+  memcpy(tmp+sizeIcmp,pdu,payload_len);
+  icmp->checksum=in_cksum((unsigned short*)tmp,sizeIcmp+payload_len);
+
+  //calculo de checksum para ip
+  u_char*temp;
+  temp=(u_char*)malloc(sizeIp+sizeIcmp+payload_len*sizeof(u_char));
+  memcpy(temp,ip,sizeIp);
+  memcpy(temp+sizeIp,icmp,sizeIcmp);
+  memcpy(temp+sizeIp+sizeIcmp,pdu,payload_len);
+  ip->check=in_cksum((unsigned short*)temp,sizeIp+sizeIcmp+payload_len);  
+
+  //construccion de paquete de respuesta
+  unsigned char packet[sizeEthernet+sizeIp+sizeIcmp+payload_len];
+  memcpy(packet,ether_h,sizeEthernet);
+  memcpy(packet+sizeEthernet,ip,sizeIp);
+  memcpy(packet+sizeEthernet+sizeIp,icmp,sizeIcmp);  
+  memcpy(packet+sizeEthernet+sizeIp+sizeIcmp,pdu,payload_len);
+
+  if(pcap_inject(pcap,packet, sizeof(packet)) == -1){
     return 0;
   }
   return 1;  
-
 }
 
-
-//fuente: http://stackoverflow.com/questions/13620607/creating-ip-network-packets
-
-unsigned short inChecksum(unsigned short * addr, int len){
-   register int sum =0;
-   u_short answer = 0;
-   register u_short *w = addr;
-   register int nleft = len;
-
+unsigned short in_cksum(unsigned short *addr, int len){
+    register int sum = 0;
+    u_short answer = 0;
+    register u_short *w = addr;
+    register int nleft = len;
+    /*
+     * Our algorithm is simple, using a 32 bit accumulator (sum), we add
+     * sequential 16 bit words to it, and at the end, fold back all the
+     * carry bits from the top 16 bits into the lower 16 bits.
+     */
     while (nleft > 1)
     {
       sum += *w++;
       nleft -= 2;
     }
-    
-
+    /* mop up an odd byte, if necessary */
     if (nleft == 1)
     {
       *(u_char *) (&answer) = *(u_char *) w;
       sum += answer;
     }
-
-    sum = (sum >> 16) + (sum & 0xffff);     
-    sum += (sum >> 16);         
-    answer = ~sum;             
-    return (answer); 
-
+    /* add back carry outs from top 16 bits to low 16 bits */
+    sum = (sum >> 16) + (sum & 0xffff);     /* add hi 16 to low 16 */
+    sum += (sum >> 16);             /* add carry */
+    answer = ~sum;              /* truncate to 16 bits */
+    return (answer);
 }
